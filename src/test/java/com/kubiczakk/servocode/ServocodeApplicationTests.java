d9acdff695938d271cb830c366ab9d1298f9164f
Endeline <henryk.kubiczak@gmail.com>
2019-04-29 17:58:42 +0200

    init commit

---
+++ src/test/java/com/kubiczakk/servocode/ServocodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kubiczakk.servocode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServocodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
